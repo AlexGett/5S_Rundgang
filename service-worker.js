c56538199f77f3b59aea282f56c711c94724d4a0
AlexGett <alex_gettinger@yahoo.com>
2026-07-30 20:19:21 +0200

    service-worker.js aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.1.0.23'; // Neue Versionsnummer
+const CACHE_VERSION = 'v1.1.0.24'; // Neue Versionsnummer
 const CACHE_NAME = `5s-rundgang-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
